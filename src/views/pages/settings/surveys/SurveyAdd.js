7d2280d5a8f9d063b9eabad0a965304df93c3a7a
Okata Ntow <okatantow@gmail.com>
2025-08-03 13:59:17 +0000

    feat:added adress to imports csv

--- src/views/pages/settings/surveys/SurveyAdd.js
+++ src/views/pages/settings/surveys/SurveyAdd.js
@@ -26,7 +26,7 @@ function SurveyAdd(props) {
   const dispatch = useDispatch();
   const [pending, setPending] = useState(false);
   const [customFields, setCustomFields] = useState([]);
-  const [surverFields, setSurveyFields] = useState(['title','description','latitute','longitude']);
+  const [surverFields, setSurveyFields] = useState(['title','description','latitute','longitude','address']);
   const [selectedUpdateRecord, setSelectedUpdateRecord] = useState();
   const [show, setShow] = useState(false);
   const [showCsv, setShowCsv] = useState(false);
