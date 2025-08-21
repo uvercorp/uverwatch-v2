c79d9b5fb5c7a292bd4d73685443353cf748f8bc
Okata Ntow <okatantow@gmail.com>
2025-08-21 15:19:52 +0000

    Update env.js

--- src/others/env.js
+++ src/others/env.js
@@ -1,12 +1,12 @@
 
 // // localhost
 // export const baseURL = "http://localhost:8000/api/";
-export const baseURL = "http://localhost/ato_data_analytics/ato-ushahidi-api/public/index.php/api/";
+// export const baseURL = "http://localhost/ato_data_analytics/ato-ushahidi-api/public/index.php/api/";
 
 
 
 
 
  //live
-// export const baseURL = "https://octopus-app-nsawa.ondigitalocean.app/api/";
+export const baseURL = "https://octopus-app-nsawa.ondigitalocean.app/api/";
 
